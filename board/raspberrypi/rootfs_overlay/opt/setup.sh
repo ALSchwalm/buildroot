2524e42f1c9957a31d943b35d18b4bc7c03b01c0
Adam Schwalm <adamschwalm@gmail.com>
2017-10-23 20:30:44 -0500

    Build lvm thin module in to kernel

--- board/raspberrypi/rootfs_overlay/opt/setup.sh
+++ board/raspberrypi/rootfs_overlay/opt/setup.sh
@@ -7,24 +7,27 @@ VOLUME_GROUP="VolGroup00"
 THINPOOL="thinpool"
 
 if [ -b $LVM_PARTITION ]; then
+    modprobe dwc2
+    modprobe libcomposite
+
     if lvs | grep $THINPOOL; then
         exit 0
     else
-        pvcreate $LVM_PARTITION
+        pvcreate $LVM_PARTITION || true
 
-        vgcreate $VOLUME_GROUP $LVM_PARTITION
+        vgcreate $VOLUME_GROUP $LVM_PARTITION || true
 
         lvcreate -l 100%FREE -T $VOLUME_GROUP/$THINPOOL
     fi
 else
-    (
+    ((
         echo n # Add a new partition
         echo p # Primary partition
         echo 3 # Partition number
         echo   # First sector (Accept default)
         echo   # Last sector (Accept default)
         echo w # Write changes
-    ) | fdisk
+    ) | fdisk /dev/mmcblk0) || true
 
     reboot
 fi
