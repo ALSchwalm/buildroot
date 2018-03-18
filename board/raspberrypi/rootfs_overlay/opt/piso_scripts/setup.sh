37d85b746ca51190a4ef9c6dd15ade14b0a1fb25
Adam Schwalm <adamschwalm@gmail.com>
2018-03-18 17:39:35 -0500

    Move configfs setup into piso

--- board/raspberrypi/rootfs_overlay/opt/piso_scripts/setup.sh
+++ board/raspberrypi/rootfs_overlay/opt/piso_scripts/setup.sh
@@ -8,39 +8,6 @@ THINPOOL="thinpool"
 CONFIGFS_HOME="/sys/kernel/config/"
 GADGET_ROOT="$CONFIGFS_HOME/usb_gadget/g1"
 
-init_configfs()
-{
-    echo "Initializing ConfigFS gadget root in '$GADGET_ROOT'"
-
-    local VENDOR_ID=0x1d6b  # Linux Foundation
-    local PRODUCT_ID=0x0104 # Multifunction Composite Gadget
-    local DEVICE_BCD=0x0100 # v1.0.0
-    local USB_BCD=0x0200    # USB2
-
-    echo $VENDOR_ID > $GADGET_ROOT/idVendor
-    echo $PRODUCT_ID > $GADGET_ROOT/idProduct
-    echo $DEVICE_BCD > $GADGET_ROOT/bcdDevice
-    echo $USB_BCD > $GADGET_ROOT/bcdUSB
-
-    mkdir -p $GADGET_ROOT/strings/0x409
-
-    local SERIAL_NUMBER="0000000000000000"
-    local MANUFACTURER="Adam Schwalm"
-    local PRODUCT="pISO"
-
-    echo $SERIAL_NUMBER > $GADGET_ROOT/strings/0x409/serialnumber
-    echo $MANUFACTURER > $GADGET_ROOT/strings/0x409/manufacturer
-    echo $PRODUCT > $GADGET_ROOT/strings/0x409/product
-
-    mkdir -p $GADGET_ROOT/configs/c.1/strings/0x409
-
-    local MAX_POWER=250
-    local CONFIGURATION="Config 1"
-
-    echo $MAX_POWER > $GADGET_ROOT/configs/c.1/MaxPower
-    echo $CONFIGURATION > $GADGET_ROOT/configs/c.1/strings/0x409/configuration
-}
-
 
 if [ -b $LVM_PARTITION ]; then
     modprobe dwc2
@@ -62,8 +29,6 @@ if [ -b $LVM_PARTITION ]; then
     # Get configfs up and running
     mount none /sys/kernel/config -t configfs
     mkdir -p $GADGET_ROOT
-
-    init_configfs
 else
     ((
         echo n # Add a new partition
