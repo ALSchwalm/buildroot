8a2274603b629ab65f51ee3df664bf3100494965
Adam Schwalm <adamschwalm@gmail.com>
2018-06-26 22:14:02 -0500

    Switch to user-mnt as root for remote access (bindfs)

--- board/piso/rootfs_overlay/opt/piso_scripts/add_user.sh
+++ board/piso/rootfs_overlay/opt/piso_scripts/add_user.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Remove any existing user that was created with a previous config
-existing=$(cat /etc/passwd | grep '/mnt' | cut -f 1 -d:)
+existing=$(cat /etc/passwd | grep '/user-mnt' | cut -f 1 -d:)
 if [ $? -eq 0 ]; then
     deluser $existing
 fi
 
-(echo "$2"; echo "$2") | adduser "$1" --home "/mnt" --no-create-home
+(echo "$2"; echo "$2") | adduser "$1" --home "/user-mnt" --no-create-home
